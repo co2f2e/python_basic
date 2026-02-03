08e6a88f0b92f43dee4d186256d90b15d0d67938
co2f2e <co2f2e@users.noreply.github.com>
2026-02-04 01:11:38 +0800

    Initial commit

---
+++ 第3章/17_赋值_复合赋值运算符.py
@@ -0,0 +1,39 @@
+age = 18
+# 加法复合运算符
+age += 1
+print(age)
+
+# 减法复合运算符
+page = 18
+page -= 1
+print(page)
+
+# 乘法符合运算符
+price = 100
+discount = 0.8
+price *= discount
+print(price)
+
+# 除法复合运算符
+pay = 100
+num = 5
+pay /= num
+print(pay)
+
+# 取整赋值运算符
+apple = 31
+num = 14
+apple //= num
+print(apple)
+
+# 取余赋值运算符
+seconds = 386
+minutes = 60
+seconds %= minutes
+print(seconds)
+
+# 指数赋值运算符
+a = 2
+b = 3
+a **= b
+print(a)
